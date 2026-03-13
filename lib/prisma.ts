af9432be0a4efa430fbb12c23392f77e58c36944
25LP7948 25LP7948 <johndoe@example.com>
2026-03-13 09:12:28 +0800

    js

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/generated/prisma/client";
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.PRISMA_DATABASE_URL;
 
 const adapter = new PrismaPg({ connectionString });
 export const prisma = new PrismaClient({ adapter });
